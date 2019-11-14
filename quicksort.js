2cf4f1c586d10c009a6cc9a8469f77857b7d2e22
nrrao <nandanarao@yahoo.com>
2019-11-14 14:14:10 -0500

    2nd Q

--- quicksort.js
+++ quicksort.js
@@ -1,5 +1,24 @@
-//3 9 1 14  17 24 22 20
-// The pivot could have been 14, but could not have been 17
+//3 9 1 14 17 24 22 20
+// The pivot could have been either 14 or 17
+
+//14, 17, 13, 15, 19, 10, 3, 16, 9, 12
+// when using the first item as pivot
+// pivot = 12
+// 9,17,13,15,19,10,3,16,14,12
+// pivot = 9
+// 3,9,10,12,19,14,17,16,13,15
+
+When using the first item on the list as a pivot
+//14, 17, 13, 15,  19, 10, 3, 16, 9, 12
+14,13,10,3,  9, 12, 15, 16, 19, 17
+
+
+
+function swap(array, i, j) {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+};
 
 function quickSort(array, start = 0, end = array.length) {
   if (start >= end) {
